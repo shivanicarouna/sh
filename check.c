c113360e90a6e73678efd80dc2a852b8c83a83a7
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-20 16:22:02 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("even");
+}
+else if(n%2!=0)
+{
+printf("odd");
+}
+else
+{
+printf("invalid");
+}
+}
