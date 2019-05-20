6c09a5054f128918e54b9bd4aecdb6893d3e517a
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-20 11:29:20 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive");
+}
+else if(n<0)
+{
+printf("Negative");
+}
+else
+{
+printf("zero");
+}
+}
