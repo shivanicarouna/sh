503b9aa08989d88e25027f566d2a9710cef737b4
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-20 22:41:50 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -4,12 +4,18 @@ void main()
 {
 char n;
 scanf("%c",&n);
-if((n=='a') ||(n=='e')||(n=='i')|| (n=='o') || (n=='u'))
+  if((n>=32)&&(n<=64) ||(n>=94)&&(n<=95)||(n>=123)&&(n<=127))
+  {
+    printf("invalid");
+  }
+  
+else if((n=='a') ||(n=='e')||(n=='i')|| (n=='o') || (n=='u'))
 {
 printf("vowel");
 }
 else
 {
-  printf("invalid");
+  printf("consonant");
 }
 }
+
