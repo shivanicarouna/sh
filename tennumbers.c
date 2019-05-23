2030175983fb7e3b4c1d23e37a7452c10e657997
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-23 12:30:25 +0530

    Update tennumbers.c

--- tennumbers.c
+++ tennumbers.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int n=10,a[200],i,t;
-scanf("%d",&n);
-for(i=0;i<n;i++)
+int p,a[10];
+for(int i=0;i<10;i++)
 {
 scanf("%d",&a[i]);
 }
 p=a[0];
-
-for(i=1;i<n;i++)
+ for(int i=0;i<10;i++)
+ {
+if(p<a[i])
 {
-if(a[i]>t)
-t=a[i];
+p=a[i];
 }
-printf("%d",t);
-
+}
+printf("%d",p);
 }
