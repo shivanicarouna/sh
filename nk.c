d4feea0d39210dddafbef6ad181449af74fd03de
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-23 14:10:07 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int a[200],i,n,k,p=0;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+    p=p+a[i];
+    
+}
+printf("%d",sum);
+}
