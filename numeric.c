cbaef8e148ede07da3c754f2636bdfd144cf32e0
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-23 09:04:28 +0530

    Update numeric.c

--- numeric.c
+++ numeric.c
@@ -1,15 +1,17 @@
+
 #include <stdio.h>
 #include<ctype.h>
-void main()
+int main()
 {
-char s;
-scanf("%c",&s);
-if(isdigit(s)
-{
-printf("yes");
-}
-else
-{
-printf("No");
-}
+    char s;
+    scanf("%c",&s);
+    if(isdigit(s))
+    {
+    printf("yes");
+    }
+    else
+    {
+    printf("No");
+    }
+ return 0;
 }
