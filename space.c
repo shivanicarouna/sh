8e728f3371a86797d615936de195d70b5df4bef8
shivanicarouna <50818594+shivanicarouna@users.noreply.github.com>
2019-05-23 10:56:37 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[200];
+    int i,count=0;
+    gets(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+    if(s[i]!=' ')
+    {
+        count++;
+    }
+   
+    }
+   printf("%d",count);
+    
+}
